6ffa003bc60fc85e9bfd7c9e32d18dbef93f977c
asus <asus@LAPTOP-BD2P4KVT>
2020-10-28 10:21:41 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan30/cakep/PBOIF210119060Latihan30Cakep.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan30.cakep;
+import java.util.Scanner;
+/**
+ *
+  @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Program CAKEP
+ */
+public class PBOIF210119060Latihan30Cakep {
+        public static final String RED = "\u001b[31m";
+        public static final String GREEN = "\u001b[32m";
+        public static final String YELLOW = "\u001b[33m";
+        public static final String BLUE = "\u001b[34m";
+        public static final String PURPLE = "\u001b[35m";
+        public static final String CYAN = "\u001b[36m";
+        
+         public static void kondisi(String hasil) {
+         if ("yoi".equals(hasil)){
+            System.out.println("");
+            System.out.println(RED+"Cakep Bener,"+PURPLE+" Good Job");
+            } else {
+                System.out.println("");
+                System.out.println(RED+"Tetep cakep sih.");
+                System.out.println(CYAN+"sing penting paham konsep nya yee.");
+                System.out.println("Keep the code works dude");
+                }
+      
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        String hasil;
+        System.out.println(RED + " Kamu" + GREEN + " ngerjain Sendiri " +YELLOW+ " Latihan 17 sampe " +BLUE + " Latihan 30 ini? " + BLUE);
+        System.out.print(BLUE + "Jawab" +RED+"(Yoi/Engga) : " );
+        hasil = input.next();
+        System.out.println("");
+        kondisi (hasil);
+         
+   }
+    
+}         
